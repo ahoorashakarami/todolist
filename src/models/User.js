3aa774b6fdc88d29fd94a19118f85eec5d2840aa
Ahoora Shakarami <ahoorashakarami91@gmail.com>
2026-08-08 22:32:57 +0330

    Added User Model

---
+++ src/models/User.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+
+const UserSchema = new mongoose.Schema(
+    {
+
+        name: {
+            type: String,
+            required: true,
+            minlength: 6,
+            maxlength: 50,
+        },
+
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+
+
+        password: {
+            type: String,
+            required: true,
+            minlength: 8,
+        },
+
+    },
+    {
+        timestamps: true,
+    }
+);
+
+
+
+const User =
+    mongoose.models.User ||
+    mongoose.model("User", UserSchema);
+
+
+export default User;
