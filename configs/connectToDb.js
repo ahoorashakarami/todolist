c8b2735feb1344bb97b3e9743270d5c9bdb0a9ff
Ahoora Shakarami <ahoorashakarami91@gmail.com>
2026-08-08 22:41:06 +0330

    Fixed connecttodb

--- configs/connectToDb.js
+++ configs/connectToDb.js
@@ -5,7 +5,7 @@ const connectToDB = async () => {
         if (mongoose.connections[0].readyState) {
             return true
         } else {
-            await mongoose.connect("mongodb://localhost:27017/next-auth")
+            await mongoose.connect("mongodb://localhost:27017/todolist-app")
             console.log("Connected To Database Successfully!")
         }
     } catch (err) {
