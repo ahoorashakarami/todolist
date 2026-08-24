058885eb87b1dc751879617b961fa45d23dbf46b
Ahoora Shakarami <ahoorashakarami91@gmail.com>
2026-08-24 16:37:38 +0330

    Added Home Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,56 @@
-import React from "react";
+import Link from "next/link";
+import styles from "./styles.module.css";
+
 
 export default function Home() {
+
   return (
-   <h1>This project is under development</h1>
+
+    <main className={styles.container}>
+
+      <div className={styles.card}>
+
+
+        <h1>
+          Welcome To Todo Manager
+        </h1>
+
+
+        <p className={styles.subtitle}>
+          Organize your tasks, stay productive, and manage your workflow smarter.
+        </p>
+
+
+
+        <Link
+          href="/signin"
+          className={styles.button}
+        >
+          Get Started
+        </Link>
+
+
+
+
+        <p className={styles.author}>
+
+          Made By{" "}
+
+          <Link
+            href="https://github.com/ahoorashakarami"
+            target="_blank"
+          >
+            Ahoora Shakarami
+          </Link>
+
+        </p>
+
+
+
+      </div>
+
+
+    </main>
+
   );
 }
